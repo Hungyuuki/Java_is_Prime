d00ca6458a89782e2dbc7b4e91164b89ad7b8782
Hungyuuki <hungyuuki777@gmail.com>
2022-01-05 12:36:12 +0700

    1

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,30 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số: ");
+        int number = scanner.nextInt();
+        if (number < 2) {
+            System.out.println(number + " is not a prime");
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check) {
+                System.out.println("Số " + number + " là số nguyên tố");
+            } else
+                System.out.println("Số " + number + " không là số nguyên tố");
+        }
+    }
+}
+
